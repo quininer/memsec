89fbecb09b4b85cb577555df593f2b967a822ed7
quininer kel <quininer@live.com>
2016-06-11 09:21:53 +0800

    init

---
+++ src/lib.rs
@@ -0,0 +1,95 @@
+#![no_std]
+
+extern crate libc;
+
+#[allow(unused_imports)]
+use libc::{
+    c_void, c_int,
+    size_t
+};
+use core::mem;
+
+
+// -- memcmp --
+
+pub unsafe fn memcmp<T>(b1: *const T, b2: *const T, len: usize) -> bool {
+    let b1 = b1 as *const u8;
+    let b2 = b2 as *const u8;
+    let mut d = 0;
+    for i in 0..len as isize {
+        d |= *b1.offset(i) ^ *b2.offset(i);
+    }
+    d == 0
+}
+
+// -- memset / memzero --
+
+#[cfg(all(unix, not(HAVE_MEMSET_S)))]
+pub unsafe fn memset<T>(s: *mut T, c: i32, n: usize) {
+    let s = s as *mut u8;
+    let mut i = 0;
+    while i < n {
+        *s.offset(i as isize) = c as u8;
+        i += 1;
+    }
+}
+
+#[cfg(HAVE_MEMSET_S)]
+pub unsafe fn memset<T>(s: *mut T, c: i32, n: usize) {
+    extern {
+        fn memset_s(s: *mut c_void, smax: size_t, c: c_int, n: size_t) -> c_int;
+    }
+    memset_s(mem::transmute(s), n, c, n);
+}
+
+
+#[cfg(all(unix, not(HAVE_EXPLICIT_BZERO)))]
+pub unsafe fn memzero<T>(dest: *mut T, n: usize) {
+    memset(dest, 0, n);
+}
+
+#[cfg(all(unix, HAVE_EXPLICIT_BZERO))]
+pub unsafe fn memzero<T>(dest: *mut T, n: usize) {
+    extern {
+        fn explicit_bzero(s: *mut c_void, n: size_t);
+    }
+    explicit_bzero(mem::transmute(dest), n);
+}
+
+#[cfg(windows)]
+pub unsafe fn memzero<T>(s: *mut T, n: usize) {
+    extern {
+        fn SecureZeroMemory(s: *mut c_void, n: size_t);
+    }
+    SecureZeroMemory(mem::transmute(s), n);
+}
+
+
+// -- mlock / munlock --
+
+#[cfg(unix)]
+pub unsafe fn mlock<T>(addr: *mut T, len: size_t) -> bool {
+    ::libc::mlock(mem::transmute(addr), len) != 0
+}
+
+#[cfg(windows)]
+pub unsafe fn mlock<T>(addr: *mut T, len: size_t) -> bool {
+    extern {
+        fn VirtualLock(addr: *mut c_void, len: size_t) -> c_int;
+    }
+    VirtualLock(mem::transmute(addr), len) != 0
+}
+
+
+#[cfg(unix)]
+pub unsafe fn munlock<T>(addr: *mut T, len: size_t) -> bool {
+    ::libc::munlock(mem::transmute(addr), len) != 0
+}
+
+#[cfg(windows)]
+pub unsafe fn munlock<T>(addr: *mut T, len: size_t) -> bool {
+    extern {
+        fn VirtualUnlock(addr: *mut c_void, len: size_t) -> c_int;
+    }
+    VirtualUnlock(mem::transmute(addr), len) != 0
+}
---
+++ tests/tests.rs
@@ -0,0 +1,26 @@
+extern crate memsec;
+
+use std::mem::size_of_val;
+
+#[test]
+fn memzero_test() {
+    let mut x: [usize; 16] = [1; 16];
+    unsafe { memsec::memzero(x.as_mut_ptr(), size_of_val(&x)) };
+    assert_eq!(x, [0; 16]);
+    x.clone_from_slice(&[1; 16]);
+    assert_eq!(x, [1; 16]);
+    unsafe { memsec::memzero(x[1..11].as_mut_ptr(), 10 * size_of_val(&x[0])) };
+    assert_eq!(x, [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1]);
+}
+
+#[test]
+fn memcmp_test() {
+    let x = [9; 16];
+    let y = [9; 17];
+    let z = [4; 16];
+
+    assert!(unsafe { memsec::memcmp(x.as_ptr(), y.as_ptr(), size_of_val(&x)) });
+    assert!(! unsafe { memsec::memcmp(x.as_ptr(), z.as_ptr(), size_of_val(&x)) });
+}
+
+// TODO how test mlock/munlock?
