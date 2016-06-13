27a8f35cea752a44cab88fc898c9bccac9b80216
quininer kel <quininer@live.com>
2016-06-13 13:04:44 +0800

    add libsodium bench

---
+++ benches/malloc.rs
@@ -0,0 +1,37 @@
+//! malloc bench
+//!
+//! Result:
+//! ```
+//! running 2 tests
+//! test libsodium_malloc ... bench:       5,789 ns/iter (+/- 13)
+//! test memsec_malloc    ... bench:       5,391 ns/iter (+/- 61)
+//! ```
+
+
+#![feature(test)]
+
+extern crate test;
+extern crate libc;
+extern crate libsodium_sys;
+extern crate memsec;
+
+use test::Bencher;
+use libc::c_void;
+
+
+#[bench]
+fn memsec_malloc(b: &mut Bencher) {
+    b.iter(|| unsafe {
+        let ptr: *mut u8 = memsec::malloc(1024);
+        memsec::free(ptr);
+    });
+}
+
+#[bench]
+fn libsodium_malloc(b: &mut Bencher) {
+    unsafe { libsodium_sys::sodium_init() };
+    b.iter(|| unsafe {
+        let ptr: *mut u8 = libsodium_sys::sodium_malloc(1024) as *mut u8;
+        libsodium_sys::sodium_free(ptr as *mut c_void);
+    });
+}
--- benches/memcmp.rs
+++ benches/memcmp.rs
@@ -2,17 +2,20 @@
 //!
 //! Result:
 //! ```
-//! running 4 tests
-//! test libc_memcmp_eq_bench ... bench:         118 ns/iter (+/- 5)
-//! test libc_memcmp_nq_bench ... bench:           5 ns/iter (+/- 2)
-//! test memcmp_eq_bench      ... bench:         113 ns/iter (+/- 25)
-//! test memcmp_nq_bench      ... bench:         113 ns/iter (+/- 17)
+//! running 6 tests
+//! test libc_memcmp_eq_bench      ... bench:         119 ns/iter (+/- 4)
+//! test libc_memcmp_nq_bench      ... bench:           5 ns/iter (+/- 0)
+//! test libsodium_memcmp_eq_bench ... bench:         602 ns/iter (+/- 16)
+//! test libsodium_memcmp_nq_bench ... bench:         605 ns/iter (+/- 15)
+//! test memsec_memcmp_eq_bench    ... bench:         113 ns/iter (+/- 1)
+//! test memsec_memcmp_nq_bench    ... bench:         113 ns/iter (+/- 1)
 //! ```
 
 #![feature(test)]
 
 extern crate test;
 extern crate libc;
+extern crate libsodium_sys;
 extern crate memsec;
 
 use test::Bencher;
@@ -21,7 +24,7 @@ use libc::c_void;
 
 
 #[bench]
-fn memcmp_eq_bench(b: &mut Bencher) {
+fn memsec_memcmp_eq_bench(b: &mut Bencher) {
     let x = [9; 1024];
     let y = [9; 1024];
 
@@ -31,12 +34,32 @@ fn memcmp_eq_bench(b: &mut Bencher) {
 }
 
 #[bench]
-fn memcmp_nq_bench(b: &mut Bencher) {
+fn memsec_memcmp_nq_bench(b: &mut Bencher) {
     let x = [8; 1024];
     let z = [3; 1024];
 
     b.iter(|| unsafe {
         memsec::memcmp(x.as_ptr(), z.as_ptr(), size_of_val(&z))
+    });
+}
+
+#[bench]
+fn libsodium_memcmp_eq_bench(b: &mut Bencher) {
+    let x = [9; 1024];
+    let y = [9; 1024];
+
+    b.iter(|| unsafe {
+        libsodium_sys::sodium_memcmp(x.as_ptr(), y.as_ptr(), size_of_val(&y))
+    });
+}
+
+#[bench]
+fn libsodium_memcmp_nq_bench(b: &mut Bencher) {
+    let x = [8; 1024];
+    let z = [3; 1024];
+
+    b.iter(|| unsafe {
+        libsodium_sys::sodium_memcmp(x.as_ptr(), z.as_ptr(), size_of_val(&z))
     });
 }
 
