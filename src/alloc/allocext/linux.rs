aceb9baee8aec6844125bd6612f92e9a281373df
Prabhpreet Dua <615318+prabhpreet@users.noreply.github.com>
2024-06-12 11:29:42 +0530

    feat: Improve memory usage- nest memfd_secret in anonymous mmap guard page
    Co-authored-by: Prabhpreet Dua <615318+prabhpreet@users.noreply.github.com>
    Co-authored-by: Karolin Varner <karo@cupdev.net>

--- src/alloc/allocext/linux.rs
+++ src/alloc/allocext/linux.rs
@@ -1,18 +1,25 @@
 extern crate std;
+use libc::{MAP_ANONYMOUS, MAP_FIXED, MAP_NORESERVE, MAP_SHARED, PROT_NONE};
+
 use self::std::process::abort;
 use crate::{alloc::*, Prot};
 use core::mem::{self, size_of};
-use core::ptr::{self, NonNull};
+use core::ptr::{self, null_mut, NonNull};
 use core::slice;
 
 use self::memfd_secret_alloc::*;
 
 mod memfd_secret_alloc {
+    use libc::{MAP_LOCKED, MAP_POPULATE};
+
     use super::*;
     use core::convert::TryInto;
 
     #[inline]
-    pub unsafe fn alloc_memfd_secret(size: usize) -> Option<(NonNull<u8>, libc::c_int)> {
+    pub unsafe fn alloc_memfd_secret(
+        size: usize,
+        ptr: Option<*mut libc::c_void>,
+    ) -> Option<(NonNull<u8>, libc::c_int)> {
         let fd: Result<libc::c_int, _> = libc::syscall(libc::SYS_memfd_secret, 0).try_into();
 
         let fd = fd.ok().filter(|&fd| fd >= 0)?;
@@ -21,10 +28,17 @@ mod memfd_secret_alloc {
         let _ = libc::ftruncate(fd, size as libc::off_t);
 
         let ptr = libc::mmap(
-            ptr::null_mut(),
+            if ptr.is_some() {
+                ptr.unwrap()
+            } else {
+                ptr::null_mut()
+            },
             size,
             Prot::ReadWrite,
-            libc::MAP_SHARED,
+            libc::MAP_SHARED
+                | if ptr.is_some() { libc::MAP_FIXED } else { 0 }
+                | MAP_LOCKED
+                | MAP_POPULATE,
             fd,
             0,
         );
@@ -47,29 +61,60 @@ unsafe fn _memfd_secret(size: usize) -> Option<*mut u8> {
         return None;
     }
 
-    // aligned alloc ptr
     let size_with_canary = CANARY_SIZE + size;
+    let front_guard_size = PAGE_SIZE + PAGE_SIZE;
     let unprotected_size = page_round(size_with_canary);
-    let total_size = PAGE_SIZE + PAGE_SIZE + unprotected_size + PAGE_SIZE;
-    let (base_ptr, fd) = alloc_memfd_secret(total_size)?;
-    let base_ptr = base_ptr.as_ptr();
-    let fd_ptr = base_ptr.add(size_of::<usize>());
-    let unprotected_ptr = base_ptr.add(PAGE_SIZE * 2);
+    let back_guard_size = PAGE_SIZE;
+    let total_size = front_guard_size + unprotected_size + back_guard_size;
 
-    // mprotect can be used to change protection flag after mmap setup
-    // https://www.gnu.org/software/libc/manual/html_node/Memory-Protection.html#index-mprotect
-    _mprotect(base_ptr.add(PAGE_SIZE), PAGE_SIZE, Prot::NoAccess);
-    _mprotect(
-        unprotected_ptr.add(unprotected_size),
-        PAGE_SIZE,
-        Prot::NoAccess,
+    let base_ptr = libc::mmap(
+        null_mut(),
+        total_size,
+        PROT_NONE,
+        MAP_SHARED | MAP_ANONYMOUS | MAP_NORESERVE,
+        -1,
+        0,
     );
+
+    if base_ptr == libc::MAP_FAILED {
+        return None;
+    }
+    let base_ptr = base_ptr as *mut u8;
+
+    let base_ptr_stored = libc::mmap(
+        base_ptr as *mut libc::c_void,
+        PAGE_SIZE,
+        Prot::ReadWrite,
+        MAP_SHARED | MAP_ANONYMOUS | MAP_FIXED,
+        -1,
+        0,
+    ) as *mut u8;
+
+    if base_ptr_stored == libc::MAP_FAILED as *mut u8 || base_ptr_stored != base_ptr {
+        libc::munmap(base_ptr as *mut libc::c_void, total_size);
+        return None;
+    }
+
+    let unprotected_ptr = base_ptr.add(front_guard_size);
+    let Some((unprotected_ptr_val, fd)) =
+        alloc_memfd_secret(unprotected_size, Some(unprotected_ptr as *mut libc::c_void))
+    else {
+        libc::munmap(base_ptr_stored as *mut libc::c_void, PAGE_SIZE);
+        libc::munmap(base_ptr as *mut libc::c_void, total_size);
+        return None;
+    };
+    assert_eq!(unprotected_ptr_val.as_ptr(), unprotected_ptr as *mut u8);
+
+    let fd_ptr = base_ptr.add(size_of::<usize>());
 
     let canary_ptr = unprotected_ptr.add(unprotected_size - size_with_canary);
     let user_ptr = canary_ptr.add(CANARY_SIZE);
     ptr::copy_nonoverlapping(CANARY.as_ptr(), canary_ptr, CANARY_SIZE);
     ptr::write_unaligned(base_ptr as *mut usize, unprotected_size);
     ptr::write_unaligned(fd_ptr as *mut libc::c_int, fd);
+
+    // mprotect can be used to change protection flag after mmap setup
+    // https://www.gnu.org/software/libc/manual/html_node/Memory-Protection.html#index-mprotect
     _mprotect(base_ptr, PAGE_SIZE, Prot::ReadOnly);
 
     assert_eq!(unprotected_ptr_from_user_ptr(user_ptr), unprotected_ptr);
@@ -109,20 +154,37 @@ pub unsafe fn free_memfd_secret<T: ?Sized>(memptr: NonNull<T>) {
     let base_ptr = unprotected_ptr.sub(PAGE_SIZE * 2);
     let fd_ptr = base_ptr.add(size_of::<usize>()) as *mut libc::c_int;
     let unprotected_size = ptr::read(base_ptr as *const usize);
+    let back_guard_ptr = unprotected_ptr.add(unprotected_size);
     let fd = ptr::read(fd_ptr);
 
-    // check
+    // check canary value is same
     if !crate::memeq(canary_ptr as *const u8, CANARY.as_ptr(), CANARY_SIZE) {
         abort();
     }
 
     // free
-    let total_size = PAGE_SIZE + PAGE_SIZE + unprotected_size + PAGE_SIZE;
-    _mprotect(base_ptr, total_size, Prot::ReadWrite);
+    let front_guard_size = PAGE_SIZE + PAGE_SIZE;
+    let back_guard_size = PAGE_SIZE;
+
+    _mprotect(base_ptr.add(PAGE_SIZE), front_guard_size, Prot::ReadWrite);
+    _mprotect(back_guard_ptr, back_guard_size, Prot::ReadWrite);
 
     crate::memzero(unprotected_ptr, unprotected_size);
 
-    let res = libc::munmap(base_ptr as *mut c_void, total_size);
+    // Unmap memfd_secret mapping
+    let res = libc::munmap(unprotected_ptr as *mut c_void, unprotected_size);
+    if res < 0 {
+        abort();
+    }
+
+    // Unmap header mapping
+    let res = libc::munmap(base_ptr as *mut c_void, PAGE_SIZE);
+    if res < 0 {
+        abort();
+    }
+
+    // Unmap reserved space mapping
+    let res = libc::munmap(base_ptr as *mut c_void, PAGE_SIZE);
     if res < 0 {
         abort();
     }
