80b9cfadbd5c117b0454243b90f9fdad5c844908
Eyob <eyobmalik@gmail.com>
2024-09-11 15:44:22 +0300

    Use Cached Page_size and test alignment function

--- src/mlock.rs
+++ src/mlock.rs
@@ -2,14 +2,12 @@
 
 #![cfg(feature = "use_os")]
 
-use libc::_SC_PAGESIZE;
+use crate::alloc::{alloc_init, ALLOC_INIT, PAGE_SIZE};
 
-/// Retrieve page size of the system
-///
-/// Used for alignment
 unsafe fn page_size() -> usize {
-    use libc::sysconf;
-    sysconf(_SC_PAGESIZE) as usize
+    ALLOC_INIT.call_once(|| alloc_init());
+
+    PAGE_SIZE
 }
 
 /// Cross-platform `mlock`.
@@ -21,13 +19,7 @@ pub unsafe fn mlock(addr: *mut u8, len: usize) -> bool {
     {
         let page_size = page_size();
 
-        // start address of the page obtained from masked the value of the page size
-        // with the memory address
-        //
-        let start_addr = (addr as usize) & !(page_size - 1);
-
-        // End address of the page
-        let end_addr = ((addr as usize) + len + page_size - 1) & !(page_size - 1);
+        let (start_addr, end_addr) = get_page_aligned_addrs(addr, len, page_size);
 
         let aligned_len = end_addr - start_addr;
 
@@ -65,15 +57,10 @@ pub unsafe fn munlock(addr: *mut u8, len: usize) -> bool {
     {
         let page_size = page_size();
 
-        // start address of the page obtained from masked the value of the page size
-        // with the memory address
-        //
-        let start_addr = (addr as usize) & !(page_size - 1);
-
-        // End address of the page
-        let end_addr = ((addr as usize) + len + page_size - 1) & !(page_size - 1);
+        let (start_addr, end_addr) = get_page_aligned_addrs(addr, len, page_size);
 
         let aligned_len = end_addr - start_addr;
+
         #[cfg(target_os = "linux")]
         libc::madvise(
             start_addr as *mut libc::c_void,
@@ -94,5 +81,34 @@ pub unsafe fn munlock(addr: *mut u8, len: usize) -> bool {
     #[cfg(windows)]
     {
         windows_sys::Win32::System::Memory::VirtualUnlock(addr.cast(), len) != 0
+    }
+}
+
+unsafe fn get_page_aligned_addrs(addr: *mut u8, len: usize, ps: usize) -> (usize, usize) {
+    // start address of the page obtained from masked the value of the page size
+    // with the memory address
+    //
+    let start_addr = (addr as usize) & !(ps - 1);
+
+    // nearest end address of the overlapping page
+    let end_addr = ((addr as usize) + len + ps - 1) & !(ps - 1);
+    (start_addr, end_addr)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_get_page_aligned_addrs_exact_page_boundary() {
+        let addr = 0x1234 as *mut u8;
+        let len = 0x1000; // 4KB
+        let test_page_size = 0x1000; // 4KB page size
+
+        unsafe {
+            let (start_addr, end_addr) = get_page_aligned_addrs(addr, len, test_page_size);
+            assert_eq!(start_addr, 0x1000);
+            assert_eq!(end_addr, 0x3000);
+        }
     }
 }
